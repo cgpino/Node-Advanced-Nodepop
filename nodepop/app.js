dd250987cd65563eb5cc4ebb08bb75f1eb4f41a2
Carlos Gómez Pino <cgpino34@gmail.com>
2018-04-02 22:28:00 +0200

    Bootstrap añadido a la web

--- nodepop/app.js
+++ nodepop/app.js
@@ -18,7 +18,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+app.engine('html', require('ejs').__express);
 
 app.locals.title = 'Nodepop';
 
